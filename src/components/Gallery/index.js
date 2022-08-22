d78ecaf1575e71dc8d64ea4bfb7fcb16174e386b
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2022-08-22 17:45:22 -0600

    added nav with usestate and useeffect

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Gallery;
